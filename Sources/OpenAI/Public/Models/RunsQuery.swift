24786a1faa8839c1d677b55d097c0d1b45932316
cpo <pointelcedric@gmail.com>
2024-02-06 10:38:17 +0100

    Add additionalInstructions in RunQuery

--- Sources/OpenAI/Public/Models/RunsQuery.swift
+++ Sources/OpenAI/Public/Models/RunsQuery.swift
@@ -10,13 +10,16 @@ import Foundation
 public struct RunsQuery: Codable {
 
     public let assistantId: String
+    public let additionalInstructions: String?
 
     enum CodingKeys: String, CodingKey {
         case assistantId = "assistant_id"
+        case additionalInstructions = "additional_instructions"
     }
     
-    public init(assistantId: String) {
+    public init(assistantId: String, additionalInstructions: String? = nil) {
 
         self.assistantId = assistantId
+        self.additionalInstructions = additionalInstructions
     }
 }
